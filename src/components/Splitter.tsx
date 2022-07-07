9ba7feccb6799eb0917ed4b2cfbd4f9659dcb404
Hal@shurabaP <harunyann@hotmail.com>
2022-07-07 23:46:30 +0900

    Impliment SplitterWrapper

--- src/components/Splitter.tsx
+++ src/components/Splitter.tsx
@@ -1,72 +1,37 @@
-import React, { useCallback, useState } from "react";
-import styled from "styled-components"
+import React, { useState } from "react";
+import SplitterWrapper from "./SplitterWrapper"
 
 type SplitterSegment = {
   width: number;
 }
 
-const SplitterGutter = styled.div.attrs<{height:number, right: number}>((props) => ({
-  style: { left: `$(props.right - 2)px` }
-}))<{height:number, right: number}>`
-  flex-shrink: 0;
-  width: 5px;
-  height: ${(props) => `${props.height}px`};
-  cursor: col-resize;
-  transition: 0.1s ease;
-  background-color: rgba(0, 0, 0, 0);
-`;
-
-const SplitterContent = styled.div.attrs<{width: number, height: number}>((props) => {
-  return {
-    style: {width: `${props.width}px`}
-  };
-})<{width: number, height: number}>`
-  height: ${(props) => `${props.height}px`};
-  flex-shrink: 0;
-  border: 0px;
-`;
-
-type SplitterWrapperProps = {
-  width: number;
-  height: number;
-  right: number;
-  onWidthChanged?: (width: number) => void;
-  onDevidedAt?: (x: number) => void;
-}
-
-const SplitterWrapper = (props: React.PropsWithChildren<SplitterWrapperProps>) => {
-    const [gutterPosition, setGutterPosition] = useState<{right: number, width: number, previousX: number}>({right: props.right, width: props.width, previousX: 0});
-
-    const handleOnDragStart=({ clientX }: React.DragEvent<HTMLDivElement>) => {
-      setGutterPosition({right: gutterPosition.right, width: gutterPosition.width, previousX: clientX});
-    }
-    const handleOnDrag = ({ clientX }: React.DragEvent<HTMLDivElement>) => {
-      const diff = clientX - gutterPosition.previousX;
-      setGutterPosition({right: gutterPosition.right + diff, width: gutterPosition.width + diff, previousX: clientX});
-    };
-
-  return  <>
-              <SplitterContent className="splitter-content" width={gutterPosition.width} height={props.height}>
-                {props.children}
-              </SplitterContent>
-              <SplitterGutter right={gutterPosition.right} height={props.height} draggable={true} onDrag={handleOnDrag} onDragStart={handleOnDragStart}/>
-            </>
-}
-
 const Splitter = (props: React.PropsWithChildren<{segments: SplitterSegment[]; height: number}>) => {
-    let right = 0;
-    const handleWidthChanged = (index: number) => (width: number) => {
-
-    }
-    return  <div style={{overflowX: "scroll", display: "flex"}}>
-              {props.children && Array.isArray(props.children) && props.children.length === props.segments.length &&
-                props.children.map((v, i) => {
-                  right += props.segments[i].width;
-                  return <SplitterWrapper right={right} width={props.segments[i].width} height={props.height} onWidthChanged={handleWidthChanged(i)} key={i}>{v}</SplitterWrapper>
-                })
-              }
-            </div>
-    ;
+  const [segments, setSegments] = useState<SplitterSegment[]>(props.segments);
+  const handleWidthChanged = (index: number) => (width: number) => {
+    const s = Array.from(segments);
+    s[index].width = width < 1 ? 1 : width;
+    setSegments(s);
+  }
+  const handleDevided = (index: number) => (x: number) => {
+    console.log(JSON.stringify({index:index, x:x}));
+  }
+  let x = 0;
+  return  <div style={{overflowX: "scroll", display: "flex", position: "relative"}}>
+            {props.children && Array.isArray(props.children) && props.children.length === props.segments.length &&
+              props.children.map((v, i) => {
+                x += segments[i].width;
+                return <SplitterWrapper
+                    x={x}
+                    width={segments[i].width}
+                    height={props.height}
+                    onWidthChanged={handleWidthChanged(i)}
+                    onDevidedAt={handleDevided(i)}
+                    key={i}
+                  >{v}</SplitterWrapper>
+              })
+            }
+          </div>
+  ;
 }
 
 export default Splitter;
---
+++ src/components/SplitterWrapper.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import styled from "styled-components"
+
+const SplitterGutter = styled.div.attrs<{x: number, height: number}>((props) => {
+  return {
+    style: {left: `${props.x - 2}px`}
+  };
+})<{x: number, height:number}>`
+  flex-shrink: 0;
+  width: 5px;
+  height: ${(props) => `${props.height}px`};
+  cursor: col-resize;
+  transition: 0.1s ease;
+  background-color: rgba(0, 0, 0, 0);
+  position: absolute;
+`;
+
+const SplitterContent = styled.div.attrs<{width: number, height: number}>((props) => {
+  return {
+    style: {width: `${props.width}px`}
+  };
+})<{width: number, height: number}>`
+  height: ${(props) => `${props.height}px`};
+  flex-shrink: 0;
+  border: 0px;
+`;
+
+type SplitterWrapperProps = {
+  width: number;
+  height: number;
+  x: number;
+  onWidthChanged?: (width: number) => void;
+  onDevidedAt?: (x: number) => void;
+}
+
+const SplitterWrapper = (props: React.PropsWithChildren<SplitterWrapperProps>) => {
+  const [dragStartX, setDragStartX] = useState(0);
+  const [diff, setDiff] = useState(0);
+
+  const handleOnDragStart=({ clientX }: React.DragEvent<HTMLDivElement>) => {
+    setDragStartX(clientX);
+  }
+  const handleOnDrag = ({ clientX }: React.DragEvent<HTMLDivElement>) => {
+    setDiff(clientX - dragStartX);
+  };
+  const handleOnDragEnd = ({ clientX }: React.DragEvent<HTMLDivElement>) => {
+    if(props.onWidthChanged) {
+      props.onWidthChanged(props.width + (clientX - dragStartX));
+    };
+    setDiff(0)
+  }
+  const handleOnDoubleClicked = (e: React.MouseEvent<HTMLDivElement>) => {
+    if(props.onDevidedAt) {
+      props.onDevidedAt(props.width - (props.x - e.clientX));
+    }
+  };
+  return  <>
+            <SplitterContent className="splitter-content" width={props.width + diff} height={props.height} onDoubleClick={handleOnDoubleClicked}>
+              {props.children}
+            </SplitterContent>
+            <SplitterGutter className="splitter-gutter" x={props.x + diff} height={props.height} draggable={true} onDrag={handleOnDrag} onDragStart={handleOnDragStart} onDragEnd={handleOnDragEnd}/>
+          </>
+}
+
+export default SplitterWrapper;
